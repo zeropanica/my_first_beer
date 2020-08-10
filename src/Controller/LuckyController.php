ff8def21c270bb5c789b8a99d9b1416e115e817c
Radu <r.tomici@pararius.nl>
2020-08-10 19:16:38 +0200

    add annotation routes for lucky number page

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,15 +3,18 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-class LuckyController extends AbstractController
+use Symfony\Component\Routing\Annotation\Route;
+class LuckyController
 {
+     /**
+     * @Route("/lucky/number")
+      */
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return $this->render('lucky/number.html.twig', [
-            'number' => $number,
-        ]);
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
     }
 }
