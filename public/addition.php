4ba68d096bd561b5308acdf11868ae68ba9a9dd2
Radu <r.tomici@pararius.nl>
2020-08-10 18:03:47 +0200

    first commit

---
+++ public/addition.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<h1>Calculators</h1>
+<form method="get">
+    <h2>Addition calculator</h2>
+    <h3>Input the two numbers you want to add together:</h3>
+    <input type="number" name="no_1"> + <input type="number" name="no_2">
+    <input type="submit" formaction="addition.php">
+    <br>
+    <h2>Division calculator</h2>
+    <h3>Input the two numbers you want to divide:</h3>
+    <input type="number" name="no_3"> - <input type="number" name="no_4">
+    <input type="submit" formaction="division.php">
+    <br>
+    <a href="index.php">Reset</a>
+</form>
+</body>
+</html>
